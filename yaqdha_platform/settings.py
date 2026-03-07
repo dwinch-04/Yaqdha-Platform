8f2aeccbc11f2d04e1d6c90a491835ff6aac966a
dwinch-04 <asmanasser507@gmail.com>
2026-03-07 03:18:45 +0300

    Update settings.py

--- yaqdha_platform/settings.py
+++ yaqdha_platform/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 """
 Django settings for yaqdha_platform project.
 
@@ -78,10 +79,10 @@ WSGI_APPLICATION = 'yaqdha_platform.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='PASTE_YOUR_LINK_HERE',
+        conn_max_age=600
+    )
 }
 
 
@@ -136,3 +137,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
