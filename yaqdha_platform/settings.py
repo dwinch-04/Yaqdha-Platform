07a99002c0129688d43b3384f70fbfa2fd8d1f68
dwinch-04 <asmanasser507@gmail.com>
2026-03-30 08:13:45 +0300

    Update settings.py

--- yaqdha_platform/settings.py
+++ yaqdha_platform/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 import os
 """
 Django settings for yaqdha_platform project.
@@ -78,10 +79,9 @@ WSGI_APPLICATION = 'yaqdha_platform.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
