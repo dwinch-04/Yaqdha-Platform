558e0dceff5d4cb8b583ce097a862dd362aa9177
dwinch-04 <asmanasser507@gmail.com>
2026-04-09 08:25:26 +0300

    Update wsgi.py

--- yaqdha_platform/wsgi.py
+++ yaqdha_platform/wsgi.py
@@ -9,12 +9,7 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yaqdha_platform.settings')
 
 application = get_wsgi_application()
-
-base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-application = WhiteNoise(application, root=os.path.join(base_dir, 'main', 'static'))
