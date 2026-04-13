d4aa77db32fbdb5db6a373c3adef19cde43c1696
dwinch-04 <asmanasser507@gmail.com>
2026-04-13 09:06:59 +0300

    Update settings.py

--- yaqdha_platform/settings.py
+++ yaqdha_platform/settings.py
@@ -72,11 +72,10 @@ WSGI_APPLICATION = 'yaqdha_platform.wsgi.application'
 #}
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
